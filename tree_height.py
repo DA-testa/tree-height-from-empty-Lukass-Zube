b98660cdcd441b17d69764084f259f84503f0e83
Lukass-Zube <lukass.zube@edu.rtu.lv>
2023-03-06 11:04:08 +0200

    commit1

--- tree_height.py
+++ tree_height.py
@@ -2,7 +2,7 @@
 
 import sys
 import threading
-import numpy
+import numpy as np
 
 
 def compute_height(n, parents):
@@ -14,7 +14,38 @@ def compute_height(n, parents):
 
 def main():
     # implement input form keyboard and from files
-    
+    inputmethod = input()
+    if inputmethod == "I":
+        nodecount = input()
+        nodestring = input()
+    elif inputmethod == "F":
+        #file = input()
+        #nodecount = file[0]
+        #nodestring = file.split(sep=' ',maxsplit=1)[1]
+
+        filename = input()
+        read = open('test/'+filename + '.txt', 'r')
+        nodecount = read.readline(0)
+        nodestring = read.readline(1)
+        read.close()
+    print(nodecount,nodestring)
+    array = np.fromstring(nodestring,sep=' ')
+    array = array.astype(int)
+    if -1 in array:
+        root = -1
+    else:
+        root = array[0]
+    highest_hight = 0
+    hight = 1
+    for i, curantelement in enumerate(array):
+        temp = curantelement
+        while temp != root:
+            temp = array[temp]
+            hight = hight + 1
+        if hight > highest_hight:
+            highest_hight = hight
+        hight = 1
+    print(highest_hight)
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
     
@@ -29,5 +60,5 @@ def main():
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
+#main()
 # print(numpy.array([1,2,3]))
